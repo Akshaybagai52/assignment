35c30161461ca630b79c19fda05ee097c2323bf1
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2024-01-15 23:08:25 +0530

    feat: making design of the cart page

--- app/screens/cart/Cart.tsx
+++ app/screens/cart/Cart.tsx
@@ -4,18 +4,20 @@ import {
   TouchableOpacity,
   Image,
   StyleSheet,
-  ScrollView,
-} from "react-native";
-import React, { useContext } from "react";
-import { CartContext } from "../../context/shop-context";
-import { Product } from "../../types/interfaces";
+  ScrollView
+} from 'react-native'
+import React, { useContext } from 'react'
+import { CartContext } from '../../context/shop-context'
+import { Product } from '../../types/interfaces'
+import { colors } from '../../styles/colors/colors'
 
 const Cart = () => {
   const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } =
-    useContext<any>(CartContext);
+    useContext<any>(CartContext)
   return (
     <ScrollView>
       <View style={styles.container}>
+        <Text>Shopping Cart</Text>
         {cartItems.map((item: Product) => (
           <View key={item.id} style={styles.itemContainer}>
             <View style={styles.itemDetails}>
@@ -25,31 +27,35 @@ const Cart = () => {
               />
               <View style={styles.textContainer}>
                 <Text style={styles.title}>{item.title}</Text>
-                <Text style={styles.price}>{item.price}</Text>
+                <Text style={styles.price}>${item.price}</Text>
               </View>
             </View>
             <View style={styles.quantityContainer}>
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => addToCart(item)}
-              >
-                <Text style={styles.buttonText}>+</Text>
-              </TouchableOpacity>
-              <Text style={styles.quantity}>{item.quantity}</Text>
               <TouchableOpacity
                 style={styles.button}
                 onPress={() => removeFromCart(item)}
               >
                 <Text style={styles.buttonText}>-</Text>
               </TouchableOpacity>
+              <Text style={styles.quantity}>{item.quantity}</Text>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => addToCart(item)}
+              >
+                <Text style={styles.buttonText}>+</Text>
+              </TouchableOpacity>
             </View>
           </View>
         ))}
         {cartItems.length > 0 ? (
-          <View style={styles.totalContainer}>
-            <Text style={styles.totalText}>Total: ${getCartTotal()}</Text>
+          <View style={styles.total}>
+            <View style={styles.totalContainer}>
+              <Text style={styles.totalText}>Total</Text>
+              <Text style={styles.totalText}> ${getCartTotal()}</Text>
+            </View>
+
             <TouchableOpacity style={styles.clearButton} onPress={clearCart}>
-              <Text style={styles.clearButtonText}>Clear Cart</Text>
+              <Text style={styles.clearButtonText}>Proceed to Checkout</Text>
             </TouchableOpacity>
           </View>
         ) : (
@@ -57,82 +63,106 @@ const Cart = () => {
         )}
       </View>
     </ScrollView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative'
   },
   itemContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 8,
+    width: '80%',
+    flex: 1,
+    paddingVertical: 20
   },
   itemDetails: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     marginRight: 8,
+    objectFit: 'contain'
   },
   thumbnail: {
-    width: 80,
-    height: 80,
+    width: 30,
+    height: 30,
     borderRadius: 8,
+    marginRight: 16
   },
   textContainer: {
     marginLeft: 8,
+    maxWidth: 150
   },
   title: {
-    fontSize: 18,
-    fontWeight: "bold",
+    fontSize: 14,
+    fontWeight: 'bold'
   },
   price: {
-    color: "#666",
+    color: '#1E222B',
+    fontSize: 14
   },
   quantityContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   button: {
-    backgroundColor: "#333",
-    padding: 8,
-    borderRadius: 4,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: colors.black10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5
   },
   buttonText: {
-    color: "white",
+    color: colors.black100, // Adjust the text color as needed
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   quantity: {
-    marginHorizontal: 8,
+    fontSize: 14,
+    marginHorizontal: 5
+  },
+  total: {
+    width: '80%',
+    paddingHorizontal: 20,
+    backgroundColor: colors.white,
+    paddingVertical: 20,
+    borderRadius: 20
   },
   totalContainer: {
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   totalText: {
-    fontSize: 18,
-    fontWeight: "bold",
+    fontSize: 14,
+    fontWeight: 'bold'
   },
   clearButton: {
-    backgroundColor: "#333",
-    padding: 8,
-    borderRadius: 4,
-    marginTop: 8,
+    backgroundColor: colors.blue60,
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 20,
+    marginTop: 30,
   },
   clearButtonText: {
-    color: "white",
+    color: 'white',
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   emptyCartText: {
     fontSize: 18,
-    fontWeight: "bold",
-    marginTop: 16,
-  },
-});
+    fontWeight: 'bold',
+    marginTop: 16
+  }
+})
 
-export default Cart;
+export default Cart
---
+++ app/styles/colors/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+    black100: '#1B262E',
+    black90: '#354349',
+    black60: '#606D76',
+    black45: '#A9B4BC',
+    black20: '#C5CDD2',
+    black10: '#E7ECF0',
+    blue60: '#2A4BA0',
+    blue80: '#153075',
+    yellow70: '#F9B023',
+    yellow50: '#FFC83A',
+    white: '#fff',
+  };
